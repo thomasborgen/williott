ee2985feb50c81de00e18d74211d64885ac6a20e
Borgen <thomasborgen91@gmail.com>
2024-06-19 22:11:27 +0300

    PWA, Home screen addable and favicon (#1)
    * Add favicon and pwa manifest
    
    * add mobile-web-app-capable meta tag

--- williott/views/base.py
+++ williott/views/base.py
@@ -1,7 +1,5 @@
 from hypermedia import (
-    Anchor,
     Body,
-    Button,
     Div,
     Doctype,
     Head,
@@ -16,7 +14,7 @@ from hypermedia.models import Element, ElementList
 from williott.html_utils import icon
 
 
-def base() -> Element:
+def real_base() -> Element:
     """Create the base page."""
     return ElementList(
         Doctype(),
@@ -25,22 +23,32 @@ def base() -> Element:
                 Title(text="Williott - Pokedex"),
                 Meta(charset="UTF-8"),
                 Meta(name="viewport", content="width=device-width, initial-scale=1.0"),
+                Meta(name="mobile-web-app-capable", content="yes"),
                 Link(rel="stylesheet", href="/static/williott.css"),
                 Link(
                     rel="stylesheet",
                     href="https://fonts.googleapis.com/icon?family=Material+Icons",
                 ),
+                Link(rel="manifest", href="/static/manifest.json"),
+                Link(rel="shortcut icon", href="/static/favicon.png", sizes="192x192"),
                 slot="head",
             ),
-            Body(
-                Div(id="content", slot="content", classes=["content"]),
-            ),
+            Body(slot="body"),
             Script(
                 src="https://unpkg.com/htmx.org@1.9.10",
                 integrity="sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC",
                 crossorigin="anonymous",
             ),
             Script(src="/static/auto_playback.js"),
+            slot="html",
             lan="en",
         ),
+    )
+
+
+def base() -> Element:
+    """Create the base page."""
+    return real_base().extend(
+        "body",
+        Div(id="content", slot="content", classes=["content"]),
     )
